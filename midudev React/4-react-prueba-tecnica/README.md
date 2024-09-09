3832e41805b295960e5b3c971ae1dc3b121ae48c
calmggfd <calmggfd@hotmail.com>
2024-09-09 18:09:22 -0300

    dia #3

--- midudev React/4-react-prueba-tecnica/README.md
+++ midudev React/4-react-prueba-tecnica/README.md
@@ -3,7 +3,8 @@ Prueba tecnica para Juniors y Trainers de React en live Coding.
 APIs:
 
 - Facts Random: https://catfact.ninja/fact
-- Image random: https://cataas.com/cat/says/hello
+- Image random: https://cataas.com/
+    - Endpoint para usar: `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
 
 - Recupera un hecho aleatorios de gatos de la primera API.
 - Recuperar la primera palabra del hecho.
--- midudev React/4-react-prueba-tecnica/src/App.jsx
+++ midudev React/4-react-prueba-tecnica/src/App.jsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react"
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
+
 export function App () {
+    const [fact, setFact]   = useState('lorem ipsum cat fact whatever')
+
+    useEffect(()    =>  {
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then(res   =>  res.json())
+        .then(data => setFact(data.fact))
+    }, [])
+
     return(
-        <h1>App de gatos</h1>
+        <main>
+            <h1>App de gatos</h1>
+            <p>{fact}</p>
+        </main>
     )
 }

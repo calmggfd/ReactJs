248cd9c41deb272399005073c6513b52563ac10d
calmggfd <calmggfd@hotmail.com>
2024-09-10 17:58:26 -0300

    dia #3 terminando :)

--- midudev React/4-react-prueba-tecnica/src/App.jsx
+++ midudev React/4-react-prueba-tecnica/src/App.jsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react"
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
 
 export function App () {
-    const [fact, setFact]   = useState('lorem ipsum cat fact whatever')
+    const [fact, setFact]   = useState()
 
     useEffect(()    =>  {
-        fetch(CAT_ENDPOINT_RANDOM_FACT)
-        .then(res   =>  res.json())
-        .then(data => setFact(data.fact))
+        async   function    getRandomFact () {
+            const res = await fetch(CAT_ENDPOINT_RANDOM_FACT)
+            const json = await res.json()
+            setFact(json.fact)
+        }
+        
+        getRandomFact()
     }, [])
 
     return(
